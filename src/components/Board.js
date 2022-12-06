a169138f0bcf33bc603a6631b2684bbfa0638386
viettrinh1617 <79010025+viettrinh1617@users.noreply.github.com>
2022-12-07 00:38:35 +0700

    first commit

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,9 +5,21 @@ export default function Board({ squares, handleClick }) {
   return (
     <div className="board">
       <div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
+        <div className="board-row">
+          <Square squares={squares} squareId="0" handleClick={handleClick}/>
+          <Square squares={squares} squareId="1" handleClick={handleClick}/>
+          <Square squares={squares} squareId="2" handleClick={handleClick}/>
+        </div>
+        <div className="board-row">
+          <Square squares={squares} squareId="3" handleClick={handleClick}/>
+          <Square squares={squares} squareId="4" handleClick={handleClick}/>
+          <Square squares={squares} squareId="5" handleClick={handleClick}/>
+        </div>
+        <div className="board-row">
+          <Square squares={squares} squareId="6" handleClick={handleClick}/>
+          <Square squares={squares} squareId="7" handleClick={handleClick}/>
+          <Square squares={squares} squareId="8" handleClick={handleClick}/>
+        </div>
       </div>
     </div>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -8,7 +8,8 @@ function Game() {
 
   //Declaring a Winner
   useEffect(() => {
-    "Your code here";
+    if(!winner){setWinner(calculateWinner(squares));}
+    //"Your code here";
   }, [squares]);
 
   //function to check if a player has won.
@@ -37,15 +38,31 @@ function Game() {
     }
     return null;
   };
-
   //Handle player
   const handleClick = (i) => {
-    "Your code here";
+    if(!winner && !squares[i]) {
+      const value = xIsNext ? "X" : "O";
+      const newSquares = [...squares];
+      newSquares[i] = value;
+      //   Why map func not working?
+      //   const newSquars = squares.map((square, index) => {
+      //   if(index === i) {return value
+      //   } else {return square; console.log("index",index); console.log("i",i);}
+      // }); 
+      setSquares(newSquares);
+      console.log("value", value);
+      console.log(squares);
+      setXIsNext(!xIsNext);
+      //"Your code here";
+      }
   };
 
   //Restart game
   const handlRestart = () => {
-    "Your code here";
+    setSquares(Array(9).fill(null));
+    setWinner("");
+    setXIsNext("true");
+    //"Your code here";
   };
 
   return (
@@ -53,9 +70,9 @@ function Game() {
       <h2 className="result">Winner is: {winner ? winner : "N/N"}</h2>
       <div className="game">
         <span className="player">Next player is: {xIsNext ? "X" : "O"}</span>
-        <Board squares={"Your code here"} handleClick={"Your code here"} />
+        <Board squares={squares} handleClick={handleClick} />
       </div>
-      <button onClick={"Your code here"} className="restart-btn">
+      <button onClick={handlRestart} className="restart-btn">
         Restart
       </button>
     </div>
